7fa2020592edb12edaaaa5d3183e5f43f0123360
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-16 21:24:14 +0100

    Farebase initial set up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ1bmT0sH0MkP1EgXrrcxq9t9UpnKTfXU",
+  authDomain: "facebook-3aab9.firebaseapp.com",
+  projectId: "facebook-3aab9",
+  storageBucket: "facebook-3aab9.appspot.com",
+  messagingSenderId: "1053383242148",
+  appId: "1:1053383242148:web:016e4045331d8eb0601131",
+  measurementId: "G-QS6XVD9JW9"
+};
