b94aa4d21aee16f9912f5a7287713245529ea46d
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-19 22:15:57 +0000

    Minor content update

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -13,21 +13,21 @@ function Feed() {
       <Post
         profilePic="https://media.gettyimages.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?s=612x612"
         message="Woop woop!"
-        timestamp="20:25"
+        timestamp="20:25 Friday"
         username="A. Striver"
         image="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA="
       />
       <Post
         profilePic="https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg"
         message="What next? Back End!"
-        timestamp="12:53"
+        timestamp="12:53 Friday"
         username="Norman Rest"
         image="https://media.istockphoto.com/photos/programming-source-code-abstract-background-picture-id1047259374?k=20&m=1047259374&s=612x612&w=0&h=pt3XbBvrmiYgoYmVzsaUeXtV8vw_jJI9Ly8P7AL6Qig="
       />
       <Post
         profilePic="https://thumbs.dreamstime.com/b/rock-guitarist-19113213.jpg"
         message="Just hanging around!"
-        timestamp="11:33"
+        timestamp="11:33 Friday"
         username="Alex Honnold"
         image="https://i.guim.co.uk/img/media/1ba9bd4ca9e43b21553fe633830acd6fa1f6071c/396_716_2259_1355/master/2259.jpg?width=1200&quality=85&auto=format&fit=max&s=79d6f3f6c2b525324a90621d47312659"
       />
