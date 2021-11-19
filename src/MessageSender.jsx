01508f83d5fda9a24b25712cd6126c858620bee9
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-19 22:27:11 +0000

    Minor Content Update

--- src/MessageSender.jsx
+++ src/MessageSender.jsx
@@ -24,7 +24,7 @@ function MessageSender() {
                <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQFjH2M4r3umhQ/profile-displayphoto-shrink_200_200/0/1629117547194?e=1642636800&v=beta&t=4u_D_k0dRgzK84Xu5-2S3DFN-zdierUdEeay9LJv4Wg"/>
               <form>
                  
-                  <input value={input} onChange={(e) => setInput(e.target.value)} className="messageSender__input" type="text" placeholder={`What's on your mind?`}/>
+                  <input value={input} onChange={(e) => setInput(e.target.value)} className="messageSender__input" type="text" placeholder={`What's on your mind, Paul?`}/>
                   <input value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} type="text" placeholder="image URL (Optional)" />
                    <button onClick={handleSubmit} type="submit">Hidden Submit</button>
               </form>
